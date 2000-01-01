cf24c3069cf596b6c9f5c2349644fadee775bdbe
Benzhi Claimant <claimant@localhost>
2000-01-01 00:00:00 +0000

    Client result for 06daf83d-2a8e-43c9-97ee-e0785b222a93-bug-5

--- internal/application/query.go
+++ internal/application/query.go
@@ -84,16 +84,9 @@ func (s *Service) Dashboard(filter CaseListFilter) (CaseDashboard, error) {
 	if filter.PerformanceFrom != nil && filter.PerformanceTo != nil && filter.PerformanceTo.Before(*filter.PerformanceFrom) {
 		return CaseDashboard{}, &Error{Code: "INVALID_PERFORMANCE_WINDOW", Message: "performanceTo 不能早于 performanceFrom", Status: 400}
 	}
-	s.dashboardMu.Lock()
-	defer s.dashboardMu.Unlock()
-	all := s.dashboardCases
-	if all == nil {
-		var err error
-		all, err = s.store.List()
-		if err != nil {
-			return CaseDashboard{}, normalize(err)
-		}
-		s.dashboardCases = all
+	all, err := s.store.List()
+	if err != nil {
+		return CaseDashboard{}, normalize(err)
 	}
 	counts := map[rigging.RiskLevel]int{rigging.RiskNormal: 0, rigging.RiskNear: 0, rigging.RiskUrgent: 0, rigging.RiskOverdue: 0, rigging.RiskReleased: 0}
 	result := CaseDashboard{Statistics: RiskStatistics{Counts: counts}}
